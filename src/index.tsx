989df56004aadea38e559499a044bbcb6e8b9007
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-03-21 16:48:25 +0100

    changed to tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import { Buffer } from 'buffer';
 
-window.Buffer = window.Buffer || Buffer;
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
 root.render(
+  <React.StrictMode>
     <App />
+  </React.StrictMode>
 );
