dba0011df40881089ba72789a5ff5ffe87273772
Vika Shcherbak <vikashcherbaka@gmail.com>
2023-04-02 22:09:36 +0200

    all tests passing

--- src/CalculatorReducer.tsx
+++ src/CalculatorReducer.tsx
@@ -32,42 +32,50 @@ const initialState: Expr | string = '';
 
 const calculatorReducer = (state: Expr | string = initialState, action: CalculatorAction) => {
   console.log(state, "STATE");
-  //console.log(action, "ACTION");
+  console.log(action, "ACTION");
   switch (action.type) {
     case 'ADD_DIGIT':
       if (typeof state === 'string') {
-        return state + action.payload;
-      } 
-      else {
-        return {
-          ...state,
-          num2: state.num2 + action.payload
+        if (state === '0' && action.payload === '0' || ((state.includes('.') || state === '') && action.payload === '.')) {
+          return state;
         }
-      } 
+        return state + action.payload;
+      } else {
+        if (state.num2 === '' || state.num2 === '0') {
+          if (action.payload === '0') {
+            return state;
+          } else if (action.payload === '.') {
+            return { ...state, num2: '0.' };
+          }
+          return { ...state, num2: action.payload };
+        } else if (action.payload === '.' && state.num2.includes('.')) {
+          return state;
+        }
+        return { ...state, num2: state.num2 + action.payload };
+      }
     case 'SET_OPERATOR':
-      if (typeof state === 'object' && state.num2 === ''  && action.payload !== '-'  ) {
+      if (typeof state === 'object' && state.num2 === '' && action.payload !== '-') {
         return {
           ...state,
           operator: action.payload
         }
-      } else if (typeof state === 'object' && state.num2 === null && action.payload === '-' ) {
+      } else if (typeof state === 'object' && state.num2 === '' && action.payload === '-') {
         return {
           ...state,
           num2: '-'
         }
-      } else if (typeof state === 'object' && state.num2 === '-' ) {
+      } else if (typeof state === 'object' && state.num2 === '-') {
         return {
           ...state,
           num2: '',
           operator: action.payload
-        }
-      }
-      else {
+        } // 5* -5
+      } else {
         return {
-              num1: state,
-              num2: '',
-              operator: action.payload
-             }
+          num1: state,
+          num2: '',
+          operator: action.payload
+        }
       }
     case 'CALCULATE_TOTAL':
       return state = String(calculateTotal(state));
